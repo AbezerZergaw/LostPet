cb027676fe5546c39055588ea898aa01728ccbc9
Shanko <tshanko@tech-res.com>
2019-08-30 09:13:06 -0400

    removed ; from main controller.java

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -10,7 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest
 import javax.validation.Valid;
 import java.io.IOException;
 import java.util.Map;
